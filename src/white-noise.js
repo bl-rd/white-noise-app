4333bb5d5c2dc19ee628f23d6f683278c363d163
Bill Reid <william.c.reid@hotmail.co.uk>
2018-07-19 13:28:55 +0100

    Initial commit of some noise!

---
+++ src/white-noise.js
@@ -0,0 +1,16 @@
+(() => {
+  var audioContext = new AudioContext();
+  var bufferSize = 2 * audioContext.sampleRate,
+    noiseBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate),
+    output = noiseBuffer.getChannelData(0);
+  for (var i = 0; i < bufferSize; i++) {
+      output[i] = Math.random() * 2 - 1;
+  }
+
+  var whiteNoise = audioContext.createBufferSource();
+  whiteNoise.buffer = noiseBuffer;
+  whiteNoise.loop = true;
+  whiteNoise.start(0);
+
+  whiteNoise.connect(audioContext.destination);
+})();
