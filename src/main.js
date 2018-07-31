cf7ffda6c17dad7975622fe3067dc8a52cd25bc4
Bill Reid <william.c.reid@hotmail.co.uk>
2018-07-31 13:50:30 +0100

    Initial layout placement

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import Noise from './pink-noise.js';
+
+const button = document.querySelector('button');
+const timer = document.querySelector('p');
+const noise = new Noise();
+// default to 45 minutes
+const minutes = 45;
+const total = 60 * 1000 * minutes;
+
+button.addEventListener('click', event => {
+  if (noise.playing) {
+    stopNoise();
+  }
+  else {
+    startNoise();
+  }
+});
+
+function updateTimePlayed() {
+  const duration = new Date(total - noise.duration());
+  const minutes = duration.getMinutes();
+  const seconds = duration.getSeconds();
+  timer.textContent = `White noise will stop in ${minutes}:${seconds}`;
+
+  if (duration.getTime() <= 0) {
+    stopNoise();
+  }
+}
+
+function stopNoise() {
+  console.log('stopping');
+  noise.stop();
+  button.textContent = '🌙';
+  window.clearInterval(updateTimePlayed);
+  timer.classList.add('hide');
+}
+
+function startNoise() {
+  console.log('starting...');
+  noise.start();
+  button.textContent = '😣';
+  window.setInterval(updateTimePlayed, 1000);
+  timer.classList.remove('hide');
+}
