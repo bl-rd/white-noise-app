461c525e254333a73390d44c0e03a6955003ba34
Bill Reid <william.c.reid@hotmail.co.uk>
2018-08-13 21:15:53 +0100

    UI updates

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Noise from './pink-noise.js';
 
 const button = document.querySelector('button');
 const timer = document.querySelector('p');
+const buttonText = document.querySelector('#button-text');
 const noise = new Noise();
 // default to 45 minutes
 const minutes = 45;
 const total = 60 * 1000 * minutes;
+const animationDuration = 1.2;
+setAnimation();
 
 button.addEventListener('click', event => {
   if (noise.playing) {
@@ -18,16 +21,20 @@ button.addEventListener('click', event => {
     // when the animation is finished then remove the button from the DOM
     button.addEventListener('animationend', e => {
       document.querySelector('body').classList.add('play');
-      setTimeout(() => button.parentElement.removeChild(button), 600);
+      buttonText.classList.add('play');
+      document.querySelector('p').classList.add('active');
+      setTimeout(() => button.parentElement.removeChild(button), animationDuration * 1000 * 1.2);
     })
   }
 });
 
 function updateTimePlayed() {
   const duration = new Date(total - noise.duration());
-  const minutes = duration.getMinutes();
-  const seconds = duration.getSeconds();
-  timer.textContent = `White noise will stop in ${minutes}:${seconds}`;
+  let minutes = duration.getMinutes();
+  let seconds = duration.getSeconds();
+  minutes = minutes < 10 ? `0${minutes.toString()}` : minutes;
+  seconds = seconds < 10 ? `0${seconds.toString()}` : seconds;
+  timer.textContent = `${minutes}:${seconds}`;
 
   if (duration.getTime() <= 0) {
     stopNoise();
@@ -37,7 +44,7 @@ function updateTimePlayed() {
 function stopNoise() {
   console.log('stopping');
   noise.stop();
-  button.textContent = '🌙';
+  // buttonText.textContent = '🌙';
   window.clearInterval(updateTimePlayed);
   timer.classList.add('hide');
 }
@@ -45,7 +52,12 @@ function stopNoise() {
 function startNoise() {
   console.log('starting...');
   noise.start();
-  button.textContent = '😣';
+  // buttonText.textContent = '😣';
   window.setInterval(updateTimePlayed, 1000);
   timer.classList.remove('hide');
+}
+
+function setAnimation() {
+  const html = document.querySelector('html');
+  html.style.setProperty('--animation-duration', `${animationDuration}s`);
 }
